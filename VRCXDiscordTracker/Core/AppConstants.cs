e7aa4ae1eaa61d8388daacebb01145922f6c31e0
Tomachi[ICHIGO] <tomachi@tomacheese.com>
2025-05-04 17:02:25 +0900

    chore: コメント追加

--- VRCXDiscordTracker/Core/AppConstants.cs
+++ VRCXDiscordTracker/Core/AppConstants.cs
@@ -1,5 +1,8 @@
 namespace VRCXDiscordTracker.Core;
 internal class AppConstants
 {
+    /// <summary>
+    /// VRCXのデフォルトのSQLiteデータベースのパス
+    /// </summary>
     public static readonly string VRCXDefaultDatabasePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "VRCX", "VRCX.sqlite3");
 }
