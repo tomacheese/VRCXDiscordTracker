362a80e3effaa2a8472428204e554e34ebb4de72
Tomachi <8929706+book000@users.noreply.github.com>
2025-08-06 20:36:23 +0900

    refactor: データベースパス設定の簡素化 (#55)
    * refactor: データベースパス設定の簡素化
    
    コンストラクタ内でのデータベースパスの設定方法を変更しました。三項演算子を使用して、条件分岐を排除し、`_databasePath`の設定を一行で行うようにしました。これにより、コードがより読みやすくなりました。
    
    * fix: 不要になった未使用の変数を削除

--- VRCXDiscordTracker/Core/VRCXDiscordTrackerController.cs
+++ VRCXDiscordTracker/Core/VRCXDiscordTrackerController.cs
@@ -36,12 +36,9 @@ internal class VRCXDiscordTrackerController : IDisposable
     public VRCXDiscordTrackerController(string databasePath)
     {
         // データベースパスが指定されていない場合は、デフォルトのVRCXデータベースパスを使用する
-        var defaultLogPath = AppConstants.VRCXDefaultDatabasePath;
-        _databasePath = databasePath ?? defaultLogPath;
-        if (string.IsNullOrEmpty(_databasePath))
-        {
-            _databasePath = defaultLogPath;
-        }
+        _databasePath = string.IsNullOrEmpty(databasePath)
+            ? AppConstants.VRCXDefaultDatabasePath
+            : databasePath;
 
         _vrcxDatabase = new VRCXDatabase(_databasePath);
 
