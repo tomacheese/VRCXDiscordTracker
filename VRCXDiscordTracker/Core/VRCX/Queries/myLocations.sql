7896b48d0e7bf7858328e20c537888638b126f40
LunaRabbit66 <kt111558@gmail.com>
2025-10-05 23:45:51 +0900

    ワールド名が重複しているときに、連続でEmbedが連続で切り替わり続ける問題を修正

--- VRCXDiscordTracker/Core/VRCX/Queries/myLocations.sql
+++ VRCXDiscordTracker/Core/VRCX/Queries/myLocations.sql
@@ -69,11 +69,18 @@ WITH
 
   locations AS (
     SELECT
-    DISTINCT location,
-      world_name,
-      world_id,
-      group_name
+      gl.location,
+      gl.world_name,
+      gl.world_id,
+      gl.group_name
     FROM gamelog_location gl
+    JOIN (
+      SELECT location, MAX(rowid) AS max_rowid
+      FROM gamelog_location
+      GROUP BY location
+    ) latest
+      ON latest.location = gl.location
+     AND gl.rowid = latest.max_rowid
   ),
 
   limited AS (
